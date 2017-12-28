b030b1e568526987a3d5413a0c607c69062cf28a
rparkchan <rparkchan@college.harvard.edu>
2017-12-28 00:06:45 -0500

    4ths and 5ths

--- scripts/gen.py
+++ scripts/gen.py
@@ -21,8 +21,8 @@ def getIntervals(scale):
 # intervals
 # SCALE = [0,2,4,7,9]
 # MAIN_INTERVALS, EXTRA_INTERVALS = getIntervals(SCALE)
-MAIN_INTERVALS = [4,5]
-EXTRA_INTERVALS = [1,1,1,2,2,2,3,5,6,6,7,7,8,9]
+MAIN_INTERVALS = [7,5]
+EXTRA_INTERVALS = [1,1,1,2,2,2,3,5,6,6,4,4,8,9]
 
 # constants
 RANGE = {"bottom": 6, "top": 15}
